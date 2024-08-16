e02c74fd8855f6f6f3b7e7caff4f32450c635b1c
Carla <carlasantos45@hotmail.com.br>
2024-08-16 02:22:42 -0300

    Tela de cadastro, referente à aula de 12/08

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import './Register.css';
 
-const Register = () => {
-return(
-    <div>
+import { InputText } from 'primereact/inputtext';
+import { Card } from 'primereact/card';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
 
-    </div> 
-);
+const Register = () => {
+    return (
+        <div className="register-container">
+            <Card title="Novo cadastro" className="card">
+                <div class="field">
+                    <InputText placeholder="Nome" />
+                </div>
+                <div class="field">
+                    <InputText placeholder="E-mail" />
+                </div>
+                <div class="flex justify-content-center">
+                    <Button label="Cadastrar" className="button"></Button>
+                </div>
+                <div class="flex justify-content-center grid mt-1">
+                    <Button label="Cancelar" size="small" severity="danger" text></Button>
+                </div>
+            </Card>
+        </div>
+    );
 }
 
 export default Register;
