899af3a23c3435ba50ab280ee73f419bd48326f5
Carla <carlasantos45@hotmail.com.br>
2024-08-16 02:06:34 -0300

    Tela de login, referente à aula de 12/08

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import DefaultLayout from './components/DefaultLayout';
+import SimpleLayout from './components/SimpleLayout';
+
 function App() {
   return (
     <>
-      <Header/>
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={Home}/>
+          <Route path='/' element={<DefaultLayout><Home/></DefaultLayout>}/>
+          <Route path='/login' element={<SimpleLayout><Login/></SimpleLayout>}/>
+          <Route path='/register' element={<SimpleLayout><Register/></SimpleLayout>}/>
         </Routes>
       </BrowserRouter>
-      <Footer/>
     </>
   );
 }
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './Login.css';
+
+import { InputText } from 'primereact/inputtext';
+import { Card } from 'primereact/card';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <Card title="Login" className="card">
+                <div class="field">
+                    <InputText placeholder="E-mail"/>
+                </div>
+                <div class="field">
+                    <Password feedback={false} placeholder="Senha"/>
+                </div>
+                <div class="flex justify-content-center">
+                    <Button label="Entrar" className="button"></Button>
+                </div>
+                <div class="flex justify-content-center grid mt-2">
+                    <Button label="Novo cadastro" size="small" severity="primary" text></Button>
+                </div>
+                <div class="flex justify-content-center grid mt-1">
+                    <Button label="Esqueci minha senha" size="small" severity="danger" text></Button>
+                </div>
+            </Card>
+        </div>
+    );
+}
+
+export default Login;
