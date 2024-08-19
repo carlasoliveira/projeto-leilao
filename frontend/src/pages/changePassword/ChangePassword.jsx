6b67e78caa65f1102b11641fbb2b4e1028022e03
Carla <carlasantos45@hotmail.com.br>
2024-08-19 19:03:27 -0300

    Validação de regras para a senha exibidas em tempo real, referente ao desafio da aula de 12/08

--- frontend/src/pages/changePassword/ChangePassword.jsx
+++ frontend/src/pages/changePassword/ChangePassword.jsx
@@ -5,8 +5,12 @@ import { InputText } from 'primereact/inputtext';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
+import { useState } from 'react';
+import PasswordChecklist from 'react-password-checklist';
 
 const ChangePassword = () => {
+    const [password, setPassword] = useState("");
+    const [passwordAgain, setPasswordAgain] = useState("");
     return (
         <div className="change-container">
             <Card title="Alterar senha" className="card">
@@ -17,12 +21,25 @@ const ChangePassword = () => {
                     <InputText placeholder="Código" />
                 </div>
                 <div class="field">
-                    <Password feedback={false} placeholder="Nova senha"/>
+                    <Password value={password} feedback={false} placeholder="Nova senha" onChange={e => setPassword(e.target.value)} />
                 </div>
                 <div class="field">
-                    <Password feedback={false} placeholder="Confirmar senha"/>
+                    <Password value={passwordAgain} feedback={false} placeholder="Confirmar senha" onChange={e => setPasswordAgain(e.target.value)} />
                 </div>
-                <div class="flex justify-content-center">
+                <PasswordChecklist
+                    rules={["minLength", "specialChar", "number", "capital", "match"]}
+                    minLength={6}
+                    value={password}
+                    valueAgain={passwordAgain}
+                    onChange={(isValid) => { }}
+                    messages={{
+                        minLength: "A senha possui no mínimo 6 caracteres.",
+                        specialChar: "A senha contém pelo menos 1 caractere especial.",
+                        number: "A senha contém pelo menos 1 número.",
+                        capital: "A senha contém pelo menos 1 letra maiúscula.",
+                        match: "As senhas são iguais.",
+                    }} />
+                <div class="flex justify-content-center grid mt-2">
                     <Button label="Alterar senha" className="button" raised link onClick={() => window.location.href = "./"}></Button>
                 </div>
                 <div class="flex justify-content-center grid mt-1">
@@ -30,6 +47,12 @@ const ChangePassword = () => {
                 </div>
             </Card>
         </div>
+
+        /*const validationPassword = (password) => {
+            return (
+
+            )
+        }*/
     );
 }
 
