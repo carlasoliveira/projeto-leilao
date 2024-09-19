2c9d794b2c06afc51a408281f06910a2fc821be5
Carla <carlasantos45@hotmail.com.br>
2024-09-19 16:43:04 -0300

    Ajustes no componente de senha

--- frontend/src/pages/changePassword/ChangePassword.jsx
+++ frontend/src/pages/changePassword/ChangePassword.jsx
@@ -1,63 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './ChangePassword.module.css';
 import { useTranslation } from 'react-i18next';
 import { InputText } from 'primereact/inputtext';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
-import { useState } from 'react';
-import PasswordChecklist from 'react-password-checklist';
+import { Divider } from 'primereact/divider';
 
 const ChangePassword = () => {
     const { t } = useTranslation();
     const [password, setPassword] = useState("");
     const [passwordAgain, setPasswordAgain] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
+    const header = <div className="font-bold mb-3">Insira a sua senha:</div>;
+    const footer = (
+        <>
+            <Divider />
+            <p className="mt-2">Insira pelo menos</p>
+            <ul className="pl-2 ml-2 mt-0 line-height-3">
+                <li>Oito caracteres</li>
+                <li>Uma letra maiúscula</li>
+                <li>Uma letra minúscula</li>
+                <li>Um caractere numérico</li>
+            </ul>
+        </>
+    );
+
+    const validatePasswords = () => {
+        if (password == passwordAgain) {
+            setPasswordError("");
+        } else {
+            setPasswordError("As senhas não coincidem");
+        }
+    };
+
     return (
         <div className={style.changeContainer}>
             <Card title={t('changePassword')} className="card md:w-25rem h-95rem lg:w-25rem h-95rem sm:w-25rem h-95rem">
-                <div class="field" className={style.field}>
+                <div className={style.field}>
                     <label htmlFor="email">{t('email')}</label><br />
                     <InputText placeholder={t('email')} />
                 </div>
-                <div class="field" className={style.field}>
+                <div className={style.field}>
                     <label htmlFor="code">{t('code')}</label><br />
                     <InputText placeholder={t('code')} />
                 </div>
-                <div class="field" className={style.field}>
+                <div className={style.field}>
                     <label htmlFor="new-password">{t('newPassword')}</label><br />
-                    <Password value={password} feedback={false} placeholder={t('newPassword')} onChange={e => setPassword(e.target.value)} />
+                    <Password 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        header={header} 
+                        footer={footer} 
+                        promptLabel="Insira a senha" 
+                        weakLabel="Fraca" 
+                        mediumLabel="Média" 
+                        strongLabel="Forte" 
+                        toggleMask 
+                        placeholder="Nova senha"
+                    />
                 </div>
-                <div class="field" className={style.field}>
-                    <label htmlFor="confirm-new-password">{t('confirmPassword')}</label><br />
-                    <Password value={passwordAgain} feedback={false} placeholder={t('confirmPassword')} onChange={e => setPasswordAgain(e.target.value)} />
+                <div className={style.field}>
+                    <label htmlFor="confirm-password">{t('confirmPassword')}</label><br />
+                    <Password 
+                        value={passwordAgain} 
+                        onChange={(e) => {
+                            setPasswordAgain(e.target.value);
+                            validatePasswords();
+                        }} 
+                        className={passwordError} 
+                        toggleMask
+                        placeholder="Confirme a nova senha"
+                    /> <p></p>
+                    {passwordError && <small className="p-error">{passwordError}</small>}
                 </div>
-                <PasswordChecklist
-                    rules={["minLength", "specialChar", "number", "capital", "match"]}
-                    minLength={6}
-                    value={password}
-                    valueAgain={passwordAgain}
-                    onChange={(isValid) => { }}
-                    messages={{
-                        minLength: "A senha possui no mínimo 6 caracteres.",
-                        specialChar: "A senha contém pelo menos 1 caractere especial.",
-                        number: "A senha contém pelo menos 1 número.",
-                        capital: "A senha contém pelo menos 1 letra maiúscula.",
-                        match: "As senhas são iguais.",
-                    }} />
-                <div class="flex justify-content-center grid mt-2">
-                    <Button label={t('changePassword')} className={style.button} raised link onClick={() => window.location.href = "./"}></Button>
-                </div>
-                <div class="flex justify-content-center grid mt-1">
-                    <Button label={t('button.cancel')} link onClick={() => window.location.href = "./login"} size="small" severity="danger" text></Button>
-                </div>
+                <Button label="Salvar" icon="pi pi-check" onClick={() => validatePasswords()} />
             </Card>
         </div>
-
-        /*const validationPassword = (password) => {
-            return (
-
-            )
-        }*/
     );
 }
 
